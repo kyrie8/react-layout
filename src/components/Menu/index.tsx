35a5b42f16c0176eb96b17d7c69d63c96a8793e1
qiujinliang <qiujinliang@tuzhanai.com>
2022-01-21 19:29:01 +0800

    f

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,26 +1,52 @@
-import React, { memo } from 'react';
+import React, { lazy, memo} from 'react';
 import { Menu } from 'antd';
-import {
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-} from '@ant-design/icons';
+import { routes } from '@/router/router'
 export interface IProps {
 
 }
 
+function formatRoutes(routes) {
+  const res = []
+  function travel(routes) {
+    routes.forEach((route) => {
+      if (route.path && !route.children) {
+        route.component = lazy(() => import(`@/views/${route.component}`))
+        res.push(route);
+      } else if (Array.isArray(route.children) && route.children.length) {
+        travel(route.children);
+      }
+    })
+  }
+  travel(routes)
+  return res
+}
+
 const MyMenu: React.FC<IProps> = (props) => {
+  const asyncRoutes = formatRoutes(routes)
+  console.log('asyncRoutes',asyncRoutes)
+  function renderMenuItem(route) {
+    return (
+      <Menu.Item icon={<route.icon/>} key={route.path}>
+        {route.name}
+      </Menu.Item>
+    )
+  }
+  function renderSubMenu(route) {
+    <Menu.SubMenu>
+      {
+        route.map(child => {
+          return child.children && child.children.length ? renderSubMenu(child) : renderMenuItem(child)
+        })
+      }
+    </Menu.SubMenu>
+  }
   return (
     <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-      <Menu.Item key="1" icon={<UserOutlined />}>
-        nav 1
-      </Menu.Item>
-      <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-        nav 2
-      </Menu.Item>
-      <Menu.Item key="3" icon={<UploadOutlined />}>
-        nav 3
-      </Menu.Item>
+      {
+        routes.map(route => {
+          return route.children && route.children.length ? renderSubMenu(route) : renderMenuItem(route)
+        })
+      }
     </Menu>
   )
 }
